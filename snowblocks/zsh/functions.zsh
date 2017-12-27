b25102e23c1cf4830e37b866ba054960f38d1981
Asemlef <asemlef@users.noreply.github.com>
2017-12-26 20:26:07 -0500

    Rework to use snowsaw for bootstrapping dotfiles

---
+++ snowblocks/zsh/functions.zsh
@@ -0,0 +1,49 @@
+# ------------------------------
+# Functions
+# ------------------------------
+# cd to root directory of git repo
+function cdg () {
+    # check if inside git repo and do nothing if not
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) != 'true' ]]; then
+        return 1
+    fi
+
+    # get root path of git repo
+    local root_path=$(git rev-parse --show-toplevel)
+
+    # ensure home directory path is correct
+    if [[ $root_path == /local/home* ]]; then
+        local root_path=${root_path/\/local\/home/\/home}
+    fi
+
+    # cd to the root of the git repo
+    cd "$root_path"
+}
+
+# update various plugins and packages
+function updateall () {
+    # vim-plug
+    if [[ -s $HOME/.vim/autoload/plug.vim ]]; then
+        print -P "%F{green}Updating vim:%f"
+        vim -c "PlugUpgrade|PlugUpdate|qa"
+        print "Update complete."
+    fi
+    # zplug
+    if [[ -d $HOME/.zplug ]]; then
+        print -P "%F{green}Updating zsh:%f"
+        zplug update
+    fi
+    # tmux tpm
+    if [[ -d $HOME/.tmux/plugins/tpm ]]; then
+        print -P "%F{green}Updating tmux:%f"
+        $HOME/.tmux/plugins/tpm/bin/install_plugins
+        $HOME/.tmux/plugins/tpm/bin/update_plugins all
+        $HOME/.tmux/plugins/tpm/bin/clean_plugins
+
+    fi
+    # homebrew
+    if hash brew 2>/dev/null; then
+        print -P "%F{green}Updating homebrew:%f"
+        brew upgrade --cleanup
+    fi
+}
