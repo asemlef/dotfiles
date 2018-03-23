a588f95c1d965fc90ccd198a829aecae54071f99
Asemlef <asemlef@users.noreply.github.com>
2018-03-23 13:54:12 -0700

    Rework aliases to overwrite system-defined aliases

--- snowblocks/zsh/aliases.zsh
+++ snowblocks/zsh/aliases.zsh
@@ -1,14 +1,6 @@
 # ------------------------------
 # Aliases
 # ------------------------------
-# use coreutils for macOS
-if [[ "$OSTYPE" == darwin* ]]; then
-    alias ls='gls'
-    alias base64='gbase64'
-    alias du='gdu'
-    alias date='gdate'
-fi
-
 # use aliases with sudo
 alias sudo='sudo '
 
@@ -17,24 +9,35 @@ if (( $+commands[nvim] )); then
     alias vim='nvim'
 fi
 
-# use pretty color output where possible
-alias ls="${aliases[ls]:-ls} --color=auto"
-alias grep="${aliases[grep]:-grep} --color=auto"
-if (( $+commands[colordiff] )); then
-    alias diff='colordiff'
+# use color with ls
+if [[ "$OSTYPE" == darwin* ]]; then
+    if (( $+commands[gls] )); then
+        alias ls="gls --color=auto"
+    else
+        alias ls="ls -G"
+    fi
+else
+    alias ls="ls --color=auto"
 fi
 
+# alternate ls alias with nice formatting
+alias ll="LC_COLLATE=C ${aliases[ls]:-ls} -lA"
+
+# format diff nicely and use color if possible
+if (( $+commands[colordiff] )); then
+    alias diff='colordiff -u'
+else
+    alias diff='diff -u'
+fi
+
+# use color with grep
+alias grep="grep --color=auto"
+
 # always prompt when removing or overwriting files
-alias rm="${aliases[rm]:-rm} -i"
-alias cp="${aliases[cp]:-cp} -i"
-alias mv="${aliases[mv]:-mv} -i"
-alias ln="${aliases[ln]:-ln} -i"
-
-# ls with proper options
-alias ll="${aliases[ls]:-ls} -lA"
-
-# format diff nicely
-alias diff="${aliases[diff]:-diff} -u"
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+alias ln="ln -i"
 
 # automatically create parent directories with mkdir
-alias mkdir="${aliases[mkdir]:-mkdir} -p"
+alias mkdir="mkdir -p"
